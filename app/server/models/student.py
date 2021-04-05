89af5eb3f27af4df4d4506a51b21ba7270079710
Thang Do Minh <dominhthang1999@gmail.com>
2021-04-06 00:12:27 +0700

    Working on UpdateStudentModel

---
+++ app/server/models/student.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr, Field
+
+
+class StudentSchema(BaseModel):
+    fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    course_of_study: str = Field(...)
+    year: int = Field(..., gt=0, lt=9)
+    gpa: float = Field(..., le=4.0)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fullname": "John Doe",
+                "email": "jdoe@x.edu.ng",
+                "course_of_study": "Water resources engineering",
+                "year": 2,
+                "gpa": "3.0",
+            }
+        }
+
+
+class UpdateStudentModel(BaseModel):
+    pass
